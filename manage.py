ed3caace6d73efd87b8bc498d4b6afcd4c62b741
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2017-02-04 21:18:34 +0100

    Remove sanity checks

--- manage.py
+++ manage.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 from flask_script import Manager
-from app import create_app, check_sanity
+from app import create_app
 
 app = create_app()
 manager = Manager(app)
-
-@manager.command
-def sanity():
-    """
-    Run a number of sanity checks and attempt to automatically
-    fix any inconsistencies.
-    """
-    check_sanity()
 
 if __name__ == "__main__":
     manager.run()
