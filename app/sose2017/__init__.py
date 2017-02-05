8306b95540399fe8fde911b0abe3d4a7859d2c92
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2017-02-05 14:52:13 +0100

    Add CustomJSONEncoder to save data informations in JSON

--- app/sose2017/__init__.py
+++ app/sose2017/__init__.py
@@ -1,7 +1,22 @@
 from flask import Blueprint, session, redirect, url_for
+from flask.json import JSONEncoder
+from datetime import date
 
 sommer17 = Blueprint('sommer17', __name__, template_folder = 'templates/')
 
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
 from . import views
 def init_app(app):
+    app.json_encoder = CustomJSONEncoder
     return app
