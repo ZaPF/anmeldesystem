14b898967c31ec474fe6fe29374b4fad3e2d05c6
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2017-04-02 17:45:44 +0200

    Implement priority handling

---
+++ app/priorities/views.py
@@ -0,0 +1,171 @@
+from . import priorities
+from flask import render_template, session, redirect, url_for, flash, current_app
+import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
+from functools import wraps
+
+class TokenForm(FlaskForm):
+    password = StringField("Token", [validators.Required()])
+
+
+def get_registrations(token):
+    s = requests.Session()
+    s.headers.update({'Authorization': 'ZaPF-Token %s' % token})
+
+    r = s.get(current_app.config['ZAPFAUTH_BASE_URL'] + 'priorities')
+
+    if r.status_code == 401:
+        raise TokenException()
+    elif r.status_code != 200:
+        raise Exception()
+
+    return r.json()
+
+
+def post_registrations(token, priorities):
+    s = requests.Session()
+    s.headers.update({'Authorization': 'ZaPF-Token %s' % token})
+
+    r = s.post(current_app.config['ZAPFAUTH_BASE_URL'] + 'priorities', json={'confirmed': priorities})
+
+    if r.status_code == 401:
+        raise TokenException()
+    elif r.status_code != 200:
+        raise Exception()
+
+    print(r.text)
+
+def get_token():
+    class TokenException(Exception):
+        pass
+
+    form = TokenForm();
+    if form.validate_on_submit():
+        token = form.password.data.upper() # FIXME: escape
+
+        try:
+            _ = get_registrations(token)
+        except TokenException:
+            form.password.data = ""
+            form.password.errors.append("Der angegebene Uni-Token ist falsch. Euer Uni-Token sollte Euch in der Einladung zugeschickt worden sein.")
+        except:
+            raise
+        else:
+            session['zapf_token'] = token
+            return redirect(url_for('priorities.index'))
+
+    return render_template('token.html', form=form)
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'zapf_token' not in session:
+            return get_token()
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@priorities.route('/', methods=['GET', 'POST'])
+@token_required
+def priorities_index():
+    registrations = get_registrations(session['zapf_token'])
+    confirmed = [registration for registration in registrations['registrations'] if registration['priority'] is not None]
+    unconfirmed = [registration for registration in registrations['registrations'] if registration['priority'] is None]
+    return render_template("priorities.html",
+            uni=registrations['uni'],
+            confirmed=confirmed,
+            unconfirmed=unconfirmed)
+
+def _generate_post_payload(priorities_dict):
+    """
+    Generate a list of priorities from a dictionary of the form {reg_id:prioritiy}
+    """
+    return sorted(
+            [k for k in priorities_dict
+                if priorities_dict[k] is not None
+                and priorities_dict[k] != -1],
+            key=lambda k: priorities_dict[k] or 0
+            )
+
+
+def priority_modifier(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        priorities = {
+                reg['reg_id']: reg['priority']
+                for reg in get_registrations(session['zapf_token'])['registrations']
+                }
+
+        post_registrations(session['zapf_token'],
+                _generate_post_payload(f(*args, priorities=priorities, **kwargs)))
+
+        return redirect(url_for('priorities.index'))
+
+    return decorated_function
+
+
+@priorities.route('/confirm/<int:id>')
+@token_required
+@priority_modifier
+def confirm(id, priorities={}):
+    if priorities[id] == -1:
+        flash('Garantierte Plätze müssen nicht bestätigt werden.')
+        return priorities
+
+    # set the confirmed registration's priority to the maximum (i.e. last) priority
+    priorities.update({id: max([v if v else 0 for v in priorities.values()])+1})
+    return priorities
+
+
+@priorities.route('/unconfirm/<int:id>')
+@token_required
+@priority_modifier
+def unconfirm(id, priorities={}):
+    if priorities[id] == -1:
+        flash('Garantierte Plätze können nicht entfernt werden.')
+        return priorities
+
+    priorities.update({id: None})
+    return priorities
+
+
+@priorities.route('/increase/<int:id>')
+@token_required
+@priority_modifier
+def increase(id, priorities={}):
+    if priorities[id] == 0:
+        flash('Das ist schon die höchste Priorität...')
+        return priorities
+    elif priorities[id] == -1:
+        flash('Garantierte Plätze können nicht umpriorisiert werden.')
+        return priorities
+
+    #To increase the priority, swap with the item directly above.
+    id_above = {priority: id for id, priority in priorities.items()}[priorities[id]-1]
+    priorities[id], priorities[id_above] = priorities[id_above], priorities[id]
+
+    return priorities
+
+@priorities.route('/decrease/<int:id>')
+@token_required
+@priority_modifier
+def decrease(id, priorities={}):
+    if priorities[id] == max([v if v else 0 for v in priorities.values()]):
+        flash('Das ist schon die niedrigste Priorität...')
+        return priorities
+    elif priorities[id] == -1:
+        flash('Garantierte Plätze können nicht umpriorisiert werden.')
+        return priorities
+
+    #To increase the priority, swap with the item directly below.
+    id_below = {priority: id for id, priority in priorities.items()}[priorities[id]+1]
+    priorities[id], priorities[id_below] = priorities[id_below], priorities[id]
+
+    return priorities
+
+@priorities.route("/logout")
+def logout():
+    session.pop('zapf_token', None)
+    return redirect(url_for('priorities.index'))
