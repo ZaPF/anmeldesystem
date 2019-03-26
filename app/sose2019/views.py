98f34ec8b38766d5c4b7b08edbb0010b7c7893bd
Hobbesgoblin <tobias.guttenberger@gmail.com>
2019-03-26 18:24:43 +0100

    bier-AK

--- app/sose2019/views.py
+++ app/sose2019/views.py
@@ -168,6 +168,7 @@ class Sommer19Registration(FlaskForm):
 #   handtuch = BooleanField('Ich möchte gerne ein Handtuch bestellen')
 #   roemer = BooleanField('Ich möchte gerne einen Weinrömer bestellen')
     krug = BooleanField('Ich möchte einen Bierkrug bestellen (ca 5€, verbindliche Kaufzusage)')
+    bierak = BoolanField('Ich möchte am Bieraustausch AK teilnehmen (5€ Teilnahmegebühr)(neuer modus, siehe email)')
     zaepfchen = SelectField('Bist du ein Zäpfchen?', choices=[
         ('ja','Ja'),
         ('jaund','Ja und ich hätte gerne einen ZaPF-Mentor.'),
